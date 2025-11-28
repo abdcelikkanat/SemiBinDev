fixed issue in the loss function regarding to minus sign

SemiBin/long_read_cluster.py
@@ -72,15 +72,15 @@ def cluster_long_read(logger, model, data, device, is_combined,
 
         if model.include_std:
             mean, vars = model.embedding(x.float())
-            # mean = mean.detach().cpu().numpy()
-            vars = np.exp(vars.detach().cpu().numpy())
-            embedding = mean
+            mean = mean.detach().cpu().numpy()
+            vars = vars.detach().cpu().numpy()
+            # embedding = mean
 
-            # samples_num = 100
-            # embedding = np.mean(
-            #     mean[:, :, np.newaxis] + np.sqrt(vars)[:, :, np.newaxis] * np.random.randn(mean.shape[0], mean.shape[1], samples_num),
-            #     axis=-1
-            # )
+            samples_num = 100
+            embedding = np.mean(
+                mean[:, :, np.newaxis] + np.sqrt(vars)[:, :, np.newaxis] * np.random.randn(mean.shape[0], mean.shape[1], samples_num),
+                axis=-1
+            )
 
         else:
             embedding = model.embedding(x.float()).detach().cpu().numpy()
@@ -117,32 +117,33 @@ def cluster_long_read(logger, model, data, device, is_combined,
     output_bin_path = os.path.join(out, 'output_bins')
 
     logger.debug('Running DBSCAN.')
-    if not model.include_std:
+    # if not model.include_std:
+    if True:
         dist_matrix = kneighbors_graph(
             embedding_new,
             n_neighbors=min(200, embedding_new.shape[0] - 1),
             mode='distance',
             p=2,
             n_jobs=args.num_process)
-    else:
-        def my_distance(idx1, idx2):
-            idx1, idx2 = int(idx1), int(idx2)
-
-            return (
-                    (embedding_new[idx1] - embedding_new[idx2])**2 * (0.25 / np.append(vars[idx1] - vars[idx2],1))
-            ).sum(axis=-1)
-
-        from sklearn.neighbors import NearestNeighbors
-
-        nbrs = NearestNeighbors(
-            n_neighbors=min(200, embedding_new.shape[0] - 1),
-            metric=my_distance,
-            n_jobs=args.num_process,
-            algorithm='brute'
-        )
-        nbrs.fit(np.arange(0, embedding_new.shape[0]).reshape(-1, 1))
-
-        dist_matrix = nbrs.kneighbors_graph(np.asarray(list(range(embedding_new.shape[0])), dtype=int)[:, np.newaxis])
+    # else:
+    #     def my_distance(idx1, idx2):
+    #         idx1, idx2 = int(idx1), int(idx2)
+    #
+    #         return (
+    #                 (embedding_new[idx1] - embedding_new[idx2])**2 * (0.25 / np.append(vars[idx1] - vars[idx2],1))
+    #         ).sum(axis=-1)
+    #
+    #     from sklearn.neighbors import NearestNeighbors
+    #
+    #     nbrs = NearestNeighbors(
+    #         n_neighbors=min(200, embedding_new.shape[0] - 1),
+    #         metric=my_distance,
+    #         n_jobs=args.num_process,
+    #         algorithm='brute'
+    #     )
+    #     nbrs.fit(np.arange(0, embedding_new.shape[0]).reshape(-1, 1))
+    #
+    #     dist_matrix = nbrs.kneighbors_graph(np.asarray(list(range(embedding_new.shape[0])), dtype=int)[:, np.newaxis])
 
     dbscan_results = []
     for eps_value in [0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55]:

SemiBin/self_supervised_model.py
@@ -25,10 +25,7 @@ def loss_function(embedding1, cov1, embedding2, cov2, label, include_std=False):
         cov1, cov2 = cov1.double(), cov2.double()
 
         # Compute the term (m_i - m_j)^2
-        mean_squared_diff = (embedding1 - embedding2) ** 2 * (0.5 / (cov1 + cov2))
-
-        # Compute the log expectation
-        d = -0.5 * (mean_squared_diff).sum(dim=1)
+        d = ( (embedding1 - embedding2)**2 * (0.25 / (cov1 + cov2 + 1e-8)) ).sum(dim=1)
 
         square_pred = torch.square(d)
         margin_square = torch.square(relu(1 - d))

SemiBin/semi_supervised_model.py
@@ -129,7 +129,7 @@ class Semi_encoding_single(torch.nn.Module):
         if not self.include_std:
             return self.encoder1(input)
         else:
-            return self.encoder1(input), self.encoder1_logcov(input)
+            return self.encoder1(input), torch.exp(self.encoder1_logcov(input))
 
     def save_with_params_to(self, path):
         torch.save({