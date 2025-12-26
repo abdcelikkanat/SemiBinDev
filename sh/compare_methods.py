94c6a7e4301f6edac81a573c1244f5b864df5ace
A.kadir <abdcelikkanat@gmail.com>
2025-12-26 21:10:08 +0100

    compare_methods.py file and Snakemake file updates

--- sh/compare_methods.py
+++ sh/compare_methods.py
@@ -1,30 +1,15 @@
 import os
 import csv
 
-base_folder_dir = "/home/cs.aau.dk/zs74qz/workspace/SemiBinICLR//outputs_iclr/fecal_deep/SemiBinICLR"
-semibin2_file = "/projects/dark_science/methylation_binning/article/outputs/nanomotif_v1.1.0/fecal_deep/SemiBin2/checkm2/quality_report.tsv"
-#"/projects/dark_science/methylation_binning/article/outputs/nanomotif_v1.1.0/fecal_deep/BaselineModel/bins/checkm2_output/quality_report.tsv"
-
-# Directories containing quality_report.csv
-epoch_list = [-1, 5, 10, 15, 20, 25, 30, 35, 40, 45]
-
-# Storage for HQ bin sets
-hq_bins_per_epoch = {}
 
 # High-quality rule – adjust if needed
 def is_high_quality(completeness, contamination):
     return completeness >= 90 and contamination <= 5
 
-for epoch in epoch_list:
-
-    if epoch > 0:
-        current_file = f"{base_folder_dir}/epoch_{epoch}/checkm2/quality_report.tsv"
-    elif epoch == -1: #Semibin2
-        current_file = semibin2_file
+def get_hq_count(current_file):
 
     if not os.path.exists(current_file):
-        print(f"[WARNING] Missing file: {current_file}")
-        continue
+        raise ValueError(f"[WARNING] Missing file: {current_file}")
 
     with open(current_file, newline="") as f:
         reader = csv.DictReader(f, delimiter='\t')
@@ -54,34 +39,16 @@ for epoch in epoch_list:
             if is_high_quality(completeness, contamination):
                 hq_bins.add(row[bin_col])
 
-        hq_bins_per_epoch[epoch] = hq_bins
+    return len(hq_bins)
 
-# --- Print summary ---
-print("=== High-Quality Bin Summary ===")
-for epoch in epoch_list:
-    bins = hq_bins_per_epoch.get(epoch, set())
-    if epoch > 0:
-        print(f"{epoch}: {len(bins)} HQ bins")
-    elif epoch == -1: #semibin2
-        print(f"SemiBin2 (15 Epochs): {len(bins)} HQ bins")
+sample_name="fecal_deep"
+base_folder_dir = f"/home/cs.aau.dk/zs74qz/workspace/SemiBinRefining/outputs_refining/{sample_name}"
+semibin2_file = f"{base_folder_dir}/SemiBin2/checkm2/quality_report.tsv"
+refining1_file = f"{base_folder_dir}/SemiBinRefining1/checkm2/quality_report.tsv"
 
-# --- Build comparison table ---
-all_bins = sorted(set().union(*hq_bins_per_epoch.values()))
 
-'''
-# Save comparison table manually
-output_file = "hq_bin_comparison.csv"
-with open(output_file, "w", newline="") as f:
-    writer = csv.writer(f)
-
-    # Header
-    writer.writerow(["Bin"] + epoch_dirs)
-
-    # Rows: bin + True/False for each epoch
-    for b in all_bins:
-        row = [b] + [b in hq_bins_per_epoch.get(epoch, set()) for epoch in epoch_dirs]
-        writer.writerow(row)
-
-print(f"\nComparison table written to: {output_file}")
-
-'''
+print(semibin2_file)
+semibin2_hq_count = get_hq_count(semibin2_file)
+refining1_hq_count = get_hq_count(refining1_file)
+print(semibin2_hq_count)
+print(refining1_hq_count)
